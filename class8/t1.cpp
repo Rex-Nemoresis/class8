0f393ced2493647d27cdeb240404ba8a62ade728
Rex-Nemoresis <53003587+Rex-Nemoresis@users.noreply.github.com>
2020-11-19 07:38:24 +0800

    Update t1.cpp

--- class8/t1.cpp
+++ class8/t1.cpp
@@ -1,25 +1,70 @@
+/*
+
 #include <iostream>
+
 #include<opencv2/opencv.hpp>
+
 using namespace cv;
+
 using namespace std;
+
+ 
+
 void main() {
     cv::Mat imgs = imread("D:\\2.jpg");
-    Mat imgc,imgd, img;
+
+    Mat imgc, imgd, img, kernel,imge;
+
     vector<vector<Point>> contours;
+
     vector<Vec4i> hierarchy;
-	//imgd.create(imgs.size(), imgs.type());
+
+       //imgd.create(imgs.size(), imgs.type());
+
     //threshold(imgs, img, 160, 255, THRESH_BINARY);
+
     cvtColor(imgs, img, COLOR_RGB2GRAY);
-    adaptiveThreshold(img, imgc, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 9,10);
+
+    //blur(img, img, Size(3, 3));
+
+    GaussianBlur(img, img, Size(3, 3), 0);
+
+    adaptiveThreshold(img, imgc, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 19,10);
+
     //adaptiveThreshold(img, imgd, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 5, 10);
-    kernel = np.ones((5, 5), np.uint8)
-    erosion = cv2.erode(img, kernel, iterations = 1)
-    imshow(".", imgc);
-	//GaussianBlur(img,img,Size(3,3),0);
-	//Canny(imgd, imgd, 3, 9, 3);
-    findContours(img,contours,hierarchy,RETR_LIST,CHAIN_APPROX_NONE);
-    drawContours(img,contours,-1,Scalar(255),-1,8,hierarchy);
-    imshow("...", img);
-	waitKey(0);
+
+    //imshow(".", imgc);
+
+    kernel = getStructuringElement(MORPH_ELLIPSE, Size(7, 7));
+
+    morphologyEx(imgc, imgd,MORPH_CLOSE, kernel);
+
+    //imshow("..", imgd);
+
+       Canny(imgd, imgd, 3, 9, 3);
+
+    findContours(imgd,contours,hierarchy,RETR_EXTERNAL,CHAIN_APPROX_NONE);
+
+    drawContours(imgd,contours,-1,Scalar(255),-1,8,hierarchy);
+
+   // imshow("...", imgd);
+
+    kernel = getStructuringElement(MORPH_ELLIPSE, Size(30, 30));
+
+    morphologyEx(imgd, imge, MORPH_OPEN, kernel);
+
+    //imshow("....", imge);
+
+    imgs.setTo(Scalar(0,255,255), imge);
+
+    imshow("....", imgs);
+
+       waitKey(0);
+
+   
+
+ 
 
 }
+
+*/
